a55467335930219dc1a3e5230f0d23500353a574
zb425 <1047441153@qq.com>
2020-09-20 22:45:50 +0800

    第一次提交

---
+++ springbootgitdemo/src/main/java/com/bgamq/springboot/SpringbootgitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bgamq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootgitdemoApplication.class, args);
+    }
+
+}
---
+++ springbootgitdemo/src/test/java/com/bgamq/springboot/SpringbootgitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgamq.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
